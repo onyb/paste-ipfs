d6e66bfb66330ff466aca5bfaa3bd422820fd5d6
Aditya Bose <adi26mar@gmail.com>
2022-09-22 21:31:10 +0530

    Add loader component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
@@ -6,8 +8,20 @@ import styles from '../styles/Home.module.css'
 
 const HomeComponent = dynamic(import('../components/home'), { ssr: false })
 
+
 export default function Home () {
-  return (
+  const [isLoading, setIsLoading] = React.useState(true);
+  
+  const handleLoading = () => {
+    setIsLoading(false);
+  }
+
+  React.useEffect(()=>{
+    window.addEventListener("load",handleLoading);
+    return () => window.removeEventListener("load", handleLoading);
+  },[])
+
+  return !isLoading ? (
     <div className={styles.container}>
       <Head>
         <title>Paste@IPFS</title>
@@ -21,6 +35,22 @@ export default function Home () {
         <HomeComponent />
       </main>
 
+      <Footer />
+    </div>
+  ) : (
+    <div className={styles.container}>
+      <Head>
+        <title>Paste@IPFS</title>
+        <meta name='description' content='Share code snippets on the Interplanetary Web' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.header}>Paste@IPFS</h1>
+        <h3 className={styles.description}>Share code snippets on the Interplanetary Web</h3>
+        <h3 className={styles.description}>Loading...</h3>
+      </main>
+      
       <Footer />
     </div>
   )
