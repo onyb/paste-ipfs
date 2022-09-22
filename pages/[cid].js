6e95b6a8660c2c7b4a269b315cc1dd33ec7098e4
Aditya Bose <adi26mar@gmail.com>
2022-09-22 21:01:24 +0530

    Add navbar with routing

--- pages/[cid].js
+++ pages/[cid].js
@@ -1,11 +1,39 @@
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import Footer from '../components/footer'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 const Viewer = dynamic(import('../components/viewer'), { ssr: false })
+
 
 export default function ViewCID () {
   const router = useRouter()
   const { cid } = router.query
 
-  return <Viewer cid={cid} />
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Paste@IPFS</title>
+        <meta name='description' content='Share code snippets on the Interplanetary Web' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        <div>
+          <h1 className={styles.navbar}>
+              <Link href="/">Paste@IPFS</Link>
+          </h1>
+          <h3 className={styles.description}>Your snippet powered by IPFS</h3>
+        </div>
+        {/* <div className={styles.main}>
+          <Viewer
+            cid={cid} />
+        </div> */}
+        <Viewer
+          cid={cid} />
+        <Footer />
+      </main>
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,13 @@ export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Paste IPFS</title>
+        <title>Paste@IPFS</title>
         <meta name='description' content='Share code snippets on the Interplanetary Web' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.header}>Paste IPFS</h1>
+        <h1 className={styles.header}>Paste@IPFS</h1>
         <h3 className={styles.description}>Share code snippets on the Interplanetary Web</h3>
         <HomeComponent />
       </main>
